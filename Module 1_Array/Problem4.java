557f47806de221d7e6f1f9973dea8b2403ae063f
GREESHMA-blip <241801360010@cutmap.ac.in>
2025-12-23 10:27:01 +0530

    Create Problem4.java

---
+++ Module 1_Array/Problem4.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) { 
+        int [] n = new int[5]  ;
+        for (int i=0;i<5;i++){
+            n[i] = i+1;
+            System.out.println( n[i]);
+        }
+        
+    }
+}
